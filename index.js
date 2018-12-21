e314a72a6eab68b402c99ee23fd518def6cbd4db
jutaaa <juthatawat.sa@gmail.com>
2018-12-21 23:42:23 +0700

    commit:jutha

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 80
+const port = 112
 const mymodel = require('./models/my_model')
+
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -11,9 +33,13 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.get('/get_all_person', (req, res) => {
-    mymodel.M_hr_person.get_all_person(function(call){
+    mymodel.M_hr_person.get_all_person(function (call) {
         res.send(JSON.stringify(call))
     })
+})
+
+app.get('/', (req, res) => {
+    res.send('Got a GET user')
 })
 
 app.get('/user', (req, res) => {
--- models/M_hr_person.js
+++ models/M_hr_person.js
@@ -6,10 +6,12 @@ mydb.con.connect(function(err){
 })//connect db
 
 var get_all_person = function(callback){
-        var sql = "SELECT * FROM hr_person";
+        var sql = "SELECT ps_fname,ps_lname,admin_name FROM `hr_person`"
+        sql += "JOIN hr_admin ON ps_admin_id = hr_admin.admin_id "
         mydb.con.query(sql,function(err,result,field){
            // console.log(result)
            callback(result)
         })
     }
 exports.get_all_person = get_all_person // export model
+
